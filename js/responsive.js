8a1b14ba035821acd64929c2871d442e020f4cd4
Ethans241 <ethan.skidmore@yahoo.com>
2025-02-11 15:57:08 +0000

    Added tip buttons to learn.html and added JS to keep the page looking clean.

---
+++ js/responsive.js
@@ -0,0 +1,112 @@
+
+// Add Section Tip Buttons
+// Add Button 1
+const btn1 = document.getElementById('theButtonAdd1');
+const myText1 = document.getElementById('addTip1');
+
+btn1.addEventListener('click', function(){
+  const addTipText1 = 'Try using your fingers to count when adding smaller numbers. For example, to solve 4 + 2, you can start at 4 and count up two more on their fingers (5, 6).';
+myText1.innerHTML = addTipText1;
+});
+
+// Add Button 2
+const btn2 = document.getElementById('theButtonAdd2');
+const myText2 = document.getElementById('addTip2');
+
+btn2.addEventListener('click', function(){
+  const addTipText2 = 'Look for numbers that add up to 10. For example, 7 + 3 equals 10, and this helps with mental math later on.';
+myText2.innerHTML = addTipText2;
+});
+
+// Add Button 3
+const btn3 = document.getElementById('theButtonAdd3');
+const myText3 = document.getElementById('addTip3');
+
+btn3.addEventListener('click', function(){
+  const addTipText3 = 'Use everyday things like counting snacks, toys, or steps. For example, "You have 3 apples, and I give you 2 more. How many do you have now?';
+myText3.innerHTML = addTipText3;
+});
+
+// Minus Section Tip Buttons
+// Minus Button 1
+const btn4 = document.getElementById('theButtonMinus1');
+const myText4 = document.getElementById('addTip4');
+
+btn4.addEventListener('click', function(){
+  const addTipText4 = 'Try using your fingers to count when adding smaller numbers. For example, to solve 4 + 2, you can start at 4 and count up two more on their fingers (5, 6).';
+myText4.innerHTML = addTipText4;
+});
+
+// Minus Button 2
+const btn5 = document.getElementById('theButtonMinus2');
+const myText5 = document.getElementById('addTip5');
+
+btn5.addEventListener('click', function(){
+  const addTipText5 = 'Look for numbers that add up to 10. For example, 7 + 3 equals 10, and this helps with mental math later on.';
+myText5.innerHTML = addTipText5;
+});
+
+// Minus Button 3
+const btn6 = document.getElementById('theButtonMinus3');
+const myText6 = document.getElementById('addTip6');
+
+btn6.addEventListener('click', function(){
+  const addTipText6 = 'Use everyday things like counting snacks, toys, or steps. For example, "You have 3 apples, and I give you 2 more. How many do you have now?';
+myText6.innerHTML = addTipText6;
+});
+
+// multiplication Section Tip Buttons
+// Times Button 1
+const btn7 = document.getElementById('theButtonTimes1');
+const myText7 = document.getElementById('addTip7');
+
+btn7.addEventListener('click', function(){
+  const addTipText7 = 'Try using your fingers to count when adding smaller numbers. For example, to solve 4 + 2, you can start at 4 and count up two more on their fingers (5, 6).';
+myText7.innerHTML = addTipText7;
+});
+
+// Times Button 2
+const btn8 = document.getElementById('theButtonTimes2');
+const myText8 = document.getElementById('addTip8');
+
+btn8.addEventListener('click', function(){
+  const addTipText8 = 'Look for numbers that add up to 10. For example, 7 + 3 equals 10, and this helps with mental math later on.';
+myText8.innerHTML = addTipText8;
+});
+
+// Times Button 3
+const btn9 = document.getElementById('theButtonTimes3');
+const myText9 = document.getElementById('addTip9');
+
+btn9.addEventListener('click', function(){
+  const addTipText9 = 'Use everyday things like counting snacks, toys, or steps. For example, "You have 3 apples, and I give you 2 more. How many do you have now?';
+myText9.innerHTML = addTipText9;
+});
+
+// Division Section Tip Buttons
+// Divide Button 1
+const btn10 = document.getElementById('theButtonDivide1');
+const myText10 = document.getElementById('addTip10');
+
+btn10.addEventListener('click', function(){
+  const addTipText10 = 'Try using your fingers to count when adding smaller numbers. For example, to solve 4 + 2, you can start at 4 and count up two more on their fingers (5, 6).';
+myText10.innerHTML = addTipText10;
+});
+
+// Divide Button 2
+const btn11 = document.getElementById('theButtonDivide2');
+const myText11 = document.getElementById('addTip11');
+
+btn11.addEventListener('click', function(){
+  const addTipText11 = 'Look for numbers that add up to 10. For example, 7 + 3 equals 10, and this helps with mental math later on.';
+myText11.innerHTML = addTipText11;
+});
+
+// divide Button 3
+const btn12 = document.getElementById('theButtonDivide3');
+const myText12 = document.getElementById('addTip12');
+
+btn12.addEventListener('click', function(){
+  const addTipText12 = 'Use everyday things like counting snacks, toys, or steps. For example, "You have 3 apples, and I give you 2 more. How many do you have now?';
+myText12.innerHTML = addTipText12;
+});
